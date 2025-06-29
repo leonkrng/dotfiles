afb7e2bab6108604ef22c4e4df83c9540723fe95
leonkrng <kroening.social@gmail.com>
2025-06-29 22:39:07 +0200

    update nvim config

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -3,6 +3,9 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   {
+    "onsails/lspkind.nvim",  
+  },
+  {
     "L3MON4D3/LuaSnip",
     dependencies = {
       "saadparwaiz1/cmp_luasnip",
@@ -13,6 +16,7 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local lspkind = require("lspkind")
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
@@ -25,16 +29,43 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),
         },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",  
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
         mapping = cmp.mapping.preset.insert({
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
+            else
+              fallback()
+            end
+          end,
+          ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end,
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
-          { name = "luasnip" }, -- For luasnip users.
+          { name = "luasnip" },
+          { name = "nvim_lsp_signature_help" },
+          { name = "path" },
         }, {
           { name = "buffer" },
         }),
@@ -42,3 +73,4 @@ return {
     end,
   },
 }
+
--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -1,43 +1,63 @@
 -- List of debug adapter: https://codeberg.org/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation
 
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-		"nvim-neotest/nvim-nio",
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    "nvim-neotest/nvim-nio",
     "mfussenegger/nvim-dap-python",
-	},
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
+  },
+  config = function()
+    local dap, dapui = require("dap"), require("dapui")
 
     require("dapui").setup()
     require("dap-python").setup("python")
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    -- C# / .NET
+    dap.adapters.coreclr = {
+      type = 'executable',
+      command = '/usr/bin/netcoredbg',
+      args = { '--interpreter=vscode' }
+    }
 
-		vim.keymap.set("n", "<F5>", function()
-			require("dap").continue()
-		end)
-		vim.keymap.set("n", "<F10>", function()
-			require("dap").step_over()
-		end)
-		vim.keymap.set("n", "<F11>", function()
-			require("dap").step_into()
-		end)
-		vim.keymap.set("n", "<F12>", function()
-			require("dap").step_out()
-		end)
-		vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, {})
-	end,
+    dap.configurations.cs = {
+      {
+        type = 'coreclr',
+        name = 'Launch - .NET Core',
+        request = 'launch',
+        program = function()
+          return vim.fn.input('Pfad zur DLL: ', vim.fn.getcwd() .. '/bin/Debug/net8.0/yourapp.dll', 'file')
+        end,
+      },
+    }
+
+    -- Automatic opening and closing of dap-ui
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    -- Keybindings
+    vim.keymap.set("n", "<F5>", function()
+      require("dap").continue()
+    end)
+    vim.keymap.set("n", "<F10>", function()
+      require("dap").step_over()
+    end)
+    vim.keymap.set("n", "<F11>", function()
+      require("dap").step_into()
+    end)
+    vim.keymap.set("n", "<F12>", function()
+      require("dap").step_out()
+    end)
+    vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, {})
+  end,
 }
