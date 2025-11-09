880e52cd3bda708df34ad6872f591647398f4912
leonkrng <kroening.social@gmail.com>
2025-11-09 23:19:55 +0100

    Automatic change between dark and lightmode at night

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -5,7 +5,14 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme "catppuccin-latte"
-    end
+
+      local hour = tonumber(os.date("%H"))
+
+      if hour >= 20 or hour < 9 then
+        vim.cmd.colorscheme("catppuccin-frappe")
+      else
+        vim.cmd.colorscheme("catppuccin-latte")
+      end
+    end,
   }
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,10 +1,19 @@
-return{
+return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    local hour = tonumber(os.date("%H"))
+    local theme
+
+    if hour >= 20 or hour < 9 then
+      theme = 'dracula'
+    else
+      theme = 'onelight'
+    end
+
     require('lualine').setup({
       options = {
-        theme = 'onelight'
-      }
+        theme = theme,
+      },
     })
-  end
+  end,
 }
