4a736e65c178fe0d762ccd5ec5f63f050afd3963
leonkrng <kroening.social@gmail.com>
2026-01-11 22:34:26 +0100

    Added blankline plugin and relative numbers

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -2,8 +2,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set number relativenumber")
 vim.g.mapleader = " "
 vim.g.background = "light"
+vim.opt.background = "light"
 
 vim.opt.swapfile = false
 
